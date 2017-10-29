6da2c1610b7fe4f446f8cfacba46466f32390901
Katsuke00 <katsuke00@disroot.org>
2017-10-29 18:30:36 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CBase - Complementary Base
+
+cbase is a set of unix utilities to complement utilchest.
+as utilchest grows this project will shrink until eventually die.
+
+projects being used:
+* heirloom: http://heirloom.sourceforge.net/
---
+++ config.mk
@@ -0,0 +1,31 @@
+## HEIRLOOM
+SHELL = /bin/sh
+
+DEFBIN = /usr/5bin
+SUSBIN = /usr/5bin/posix
+SU3BIN = /usr/5bin/posix2001
+UCBBIN = /usr/ucb
+CCSBIN = /usr/ccs/bin
+DFLDIR = /etc/default
+
+DEFS  = -DDEFAULT='"$(DFLDIR)/ps"' -DSUDFL='"$(DFLDIR)/su"'\
+	-DTARDFL='"$(DFLDIR)/tar"' -DSHELL='"$(SHELL)"'
+
+PATHS = -DSUSBIN='"$(SUSBIN)"' -DSU3BIN='"$(SU3BIN)"' -DDEFBIN='"$(DEFBIN)"'\
+	-DUCBBIN='"$(UCBBIN)"' -DCCSBIN='"$(CCSBIN)"' -DSV3BIN='$(SV3BIN)'
+
+STDS  = -DUCB -DSUS
+
+CPPFLAGS += -D_GNU_SOURCE $(DEFS) $(PATHS) $(STDS)
+
+## DEFAULT
+AR     = ar
+CC     = musl-gcc
+YACC   = bison -d -y
+RANLIB = ranlib
+
+CPPFLAGS += -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_FILE_OFFSET_BITS=64
+CFLAGS   += -std=c99 -Wall -pedantic
+LDFLAGS  += -Os -static
+
+PREFIX = /usr/local
